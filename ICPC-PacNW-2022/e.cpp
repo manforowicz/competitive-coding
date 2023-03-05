969bbbdcbe0f7b00a6ff9806d02d052b54323bb5
Marcin Anforowicz <manforowicz@gmail.com>
2023-03-05 15:37:16 -0800

    Changes

--- ICPC-PacNW-2022/e.cpp
+++ ICPC-PacNW-2022/e.cpp
@@ -9,8 +9,24 @@ struct Color {
 	int color, i;
 };
 
-// need to know where balls of color are
-// need to know what balls are at position
+// position of the color
+vector<vector<Pos>> pos_of_color; // [color][i]
+
+// color of the position
+vector<vector<Color>> color_of_pos; // [tube][height]
+
+
+void add_ball(int color, int tube, int height) {
+	if (color == 0) {
+		return;
+	}
+	pos_of_color[color].push_back(Pos{tube, height});
+	int color_i = (int) pos_of_color.size() - 1;
+	color_of_pos[tube].push_back(Color{color, color_i});
+}
+
+void move(int from_tube, int to_tube) {
+}
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -18,18 +34,37 @@ int main() {
 	
 	int n; cin >> n;
 	
-	vector<vector<Pos>> colors(n+1); // [color][i]
+	// position of the color
+	vector<vector<Pos>> pos_of_color(n+1); // [color][i]
 	
-	vector<vector<Color>> positions(n+2, vector<int>(3)); // [tube][height]
+	// color of the position
+	vector<vector<Color>> color_of_pos(n+2); // [tube][height]
 	
 	
-	for (int i = 0; i <= n; i++) {
+	// fill vectors
+	for (int tube = 0; tube < n+1; tube++) {
 		int b, m, t; cin >> b >> m >> t;
+		add_ball(b, tube, 0);
+		add_ball(m, tube, 1);
+		add_ball(t, tube, 2);
+	}
+	
+	// empty first tube
+	for (int color_of_pos[0].size()-1; i >= 0; i--) {
 		
-		positions[i][0] = {b, 0};
+		// loop over all tubes, checking for empty
+		for (size_t tube = 0; tube < color_of_pos.size(); tube++) {
+			
+			// check if tube is empty
+			if (find(color_of_pos[tube].begin(), color_of_pos[tube].end(), 0) != color_of_pos[tube].end())  {
+			}
+		}
+	}
+				
 		
-		colors[b].push_back({i, 0}):;
-		colors[m].push_back({i, 1});
-		colors[t].push_back({i, 2});
+	
+	for (int color = 1; color <= n; color++) {
+		
+		
 	}
 }
