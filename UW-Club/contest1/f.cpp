bef70c5511f28617c945e9586c0190fc5e9305d0
manforowicz <manforowicz@gmail.com>
2023-04-03 07:24:31 -0700

    Started F

---
+++ UW-Club/contest1/f.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	freopen("in.txt", "r", stdin);
+	
+	int n, q; cin >> n >> q;
+	
+	// MAKE SEGMENT TREE
+	
+	
+	for (int i = 0; i < q; i++) {
+		int l, r; cin >> l >> r;
+		// TODO
+	}
+}
