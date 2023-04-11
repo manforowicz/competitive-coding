b9833dcdc270d305a3b884a40d7ce64205946424
manforowicz <manforowicz@gmail.com>
2023-04-11 09:55:28 -0700

    Py helper

--- UW-Club/contest1/help.py
+++ UW-Club/contest1/help.py
@@ -1,16 +1,18 @@
-for n in range(100):
-	count = 0
-	for b in range(2, n+1):
-		tmp = n
-		valid = 1
-		while tmp > 0:
-			if tmp % b != 0 and tmp % b != 1:
-				valid = 0
-				break
-			tmp //= b
-		count += valid
-	
-	print("n:", n, "count", count, end = '')
-	for i in range(count):
-		print('-', end='')
-	print()
+n = 36
+
+for b in range(2, n+1):
+	tmp = n
+	digits = []
+	while tmp > 0:
+		digits.append(tmp % b)
+		tmp //= b
+	digits.reverse()
+
+	print(digits)
+	if digits.count(1) + digits.count(0) == len(digits):
+		print("woho")
+
+
+	# for i in range(n // b % b):
+		# print('-', end='')
+	# print()
