c1a956cbde85a00c272d4775b84ac7ad065ee7b5
manforowicz <manforowicz@gmail.com>
2023-05-08 20:19:42 -0700

    Stuck on E runtime error

--- club/contest6/rust/src/bin/e.rs
+++ club/contest6/rust/src/bin/e.rs
@@ -1,8 +1,5 @@
-#![allow(unused_imports, dead_code)]
-use std::cmp::{max, min};
-use std::collections::*;
-use std::fs::File;
-use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
+//#![allow(unused_imports, dead_code)]
+use std::io::{stdin, stdout, BufRead, BufWriter, Write};
 use std::str::{FromStr, SplitAsciiWhitespace};
 
 struct Read<T: BufRead> {
@@ -39,6 +36,35 @@ macro_rules! scan {
     }}
 }
 
+// returns number of elements which must be incremented
+fn num_to_increment(arr: &[usize], i: usize, mex: usize) -> usize {
+    if arr[i] == mex - 1 {
+        return 0;
+    }
+
+    let mut left = 0;
+    let mut right = i - 1;
+    loop {
+        let mid = (left + right) / 2;
+        if mex - 1 - arr[mid] > i - mid {
+            right = mid - 1;
+        } else if mex - 1 - arr[mid] < i - mid {
+            left = mid + 1;
+        } else if mex - 1 - arr[mid] == i - mid {
+            return i - mid;
+        }
+    }
+}
+
+// counts number of val starting at index start
+fn count_from(arr: &[usize], start: usize, val: usize) -> usize {
+    let mut end = start;
+    while end < arr.len() && arr[end] == val {
+        end += 1;
+    }
+    end - start
+}
+
 fn main() {
     let stdin = stdin();
     let stdout = stdout();
@@ -47,14 +73,32 @@ fn main() {
 
     let t = scan!(read, u32);
     for _ in 0..t {
-        //let n = scan!(read, u32);
+        let n = scan!(read, usize);
         let mut arr = read.next_arr::<usize>();
         arr.sort_unstable();
-        for (i, elem) in arr.iter().enumerate() {
-            if *elem > i {
-                write!(out, "-1").unwrap();
-            } else {
+
+        write!(out, "{} ", count_from(&arr, 0, 0)).unwrap();
+
+        let mut added = 0;
+
+        let mut i = 0;
+        let mut impossible = false;
+        for mex in 1..=n {
+            while i + 1 < n && arr[i + 1] < mex {
+                i += 1;
             }
+            if i < mex - 1 {
+                impossible = true;
+            }
+            if impossible {
+                write!(out, "-1 ").unwrap();
+                continue;
+            }
+
+            added += num_to_increment(&arr, i, mex);
+
+            write!(out, "{} ", added + count_from(&arr, i + 1, mex)).unwrap();
         }
+        writeln!(out).unwrap();
     }
 }
